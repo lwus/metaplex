073f68d9104bfff56f06c99f99a36b447136ca11
Lawrence Wu <lawrencejwu@gmail.com>
2021-11-24 11:30:59 -0600

    asyncart: copy template for client lever website

---
+++ js/packages/asyncart/src/utils/ids.ts
@@ -0,0 +1,13 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const ASYNCART_PROGRAM_ID = new PublicKey(
+  'Ax22eZWmvg77HAE5eWbvhdzZYLPmv6C8TV28ivasjB5L',
+);
+
+export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+);
+
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+);
